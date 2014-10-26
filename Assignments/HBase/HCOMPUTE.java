af59c5f5b0fe8987d722773e74f4ff291f277d12
Jinlei Kuang <jinlei.kuang@gmail.com>
2014-10-26 10:50:00 -0400

    update with the correct file

--- Assignments/HBase/HCOMPUTE.java
+++ Assignments/HBase/HCOMPUTE.java
@@ -1,137 +1,110 @@
-package HPOPULATE;
+package HCOMPUTE;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
-import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
-import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-import au.com.bytecode.opencsv.CSVParser;
-
-public class HPOPULATE {
+public class HCOMPUTE {
 	
 	private static String tablename = "AIRLINE_DELAY";
 	private static String delayFamily = "DELAYMINUTES";
+	private static byte[] DELAYFAMILY = Bytes.toBytes(delayFamily);
+	private static byte[] DELAYCOLUMN = Bytes.toBytes("delay");
+	private static byte[] STARTROWKEY = Bytes.toBytes("20080");
+	private static byte[] ENDROWKEY = Bytes.toBytes("20081");
 	
-	public static class PopulateMapper extends 
-			Mapper<LongWritable, Text, ImmutableBytesWritable, DoubleWritable>{
-		private ImmutableBytesWritable keyOut = new ImmutableBytesWritable();
-		private DoubleWritable valueOut = new DoubleWritable();
+	public static class ComputeMapper extends TableMapper<Text, Text>{
+		private Text keyOut = new Text();
+		private Text valOut = new Text();
 		@Override
-		public void map(LongWritable keyin, Text value, Context context) 
+		public void map(ImmutableBytesWritable rowkey, Result values, Context context) 
 				throws IOException, InterruptedException{
-			//retrieve the needed value from the current line
-			CSVParser parser = new CSVParser();
-			String nextLine[] = parser.parseLine(value.toString());
-			String year = nextLine[0];
-			String month = nextLine[2];
-			String airlineId = nextLine[7];
-			double arrDelayMinutes;
-			String isCancelled = nextLine[41];
-			String isDiverted = nextLine[43];
-			String isDeparted;
-			
-			if(month.length() == 1){
-				month = "0" + month;
+			String airlineID = Bytes.toString(rowkey.get()).substring(5, 10);
+			String month = Bytes.toString(rowkey.get()).substring(10, 12);
+			//get rid of "0" in front of month
+			if(month.startsWith("0")){
+				month = month.substring(1);
 			}
-			
-			if(isCancelled.equals("0.00") & isDiverted.equals("0.00")){
-				isDeparted = "0";
-				arrDelayMinutes = Double.parseDouble(nextLine[37]);
-			} else {
-				isDeparted = "1";
-				arrDelayMinutes = 0.00;
-			}
-			//generate rowkey
-			String rowString = year + isDeparted + airlineId + month;
-			byte[] rowkey = rowString.getBytes();
-			keyOut.set(rowkey);
-			valueOut.set(arrDelayMinutes);
-			context.write(keyOut, valueOut);
-		}		
-	}
-	
-	public static class PopulateReducer extends 
-			TableReducer<ImmutableBytesWritable, DoubleWritable, ImmutableBytesWritable>{
-		@Override
-		public void reduce(ImmutableBytesWritable keyin, 
-				Iterable<DoubleWritable> values, Context context) 
-				throws IOException, InterruptedException{
-			int count = 1;
-			for(DoubleWritable value : values){
-				byte[] rowKey = rowkeyConverter(keyin.get(), count);
-				Put put = new Put(rowKey);
-				put.add(
-						Bytes.toBytes(delayFamily), 
-						Bytes.toBytes("delay"), 
-						Bytes.toBytes(value.get()));
-				context.write(new ImmutableBytesWritable(rowKey), put);
-				count ++;
-			}
-		}
-		
-		protected byte[] rowkeyConverter(byte[] prefix, int count){
-			byte[] rowkey = new byte[prefix.length + Bytes.SIZEOF_INT];
-			Bytes.putBytes(rowkey, 0, prefix, 0, prefix.length);
-			Bytes.putInt(rowkey, prefix.length, count);
-			return rowkey;
+			Double value = Bytes.toDouble(values.getValue(DELAYFAMILY, DELAYCOLUMN));
+			keyOut.set(airlineID);
+			valOut.set(month + ":" + value);
+			context.write(keyOut, valOut);
 		}
 	}
 	
-	public static int run(String[] args) 
-			throws IOException, InterruptedException, ClassNotFoundException{
+	public static class ComputeReducer extends Reducer<Text, Text, NullWritable, Text>{
+		private NullWritable nullkey = NullWritable.get();
+		@Override
+		public void reduce(Text keyin, Iterable<Text> values, Context context) 
+				throws IOException, InterruptedException{
+			String resultString = keyin.toString();
+			double sum[] = new double[12];
+			Arrays.fill(sum, 0.00);
+			int count[] = new int[12];
+			double average[] = new double[12];
+			int month;
+			String line[];
+			//caculate the sum and count of the delayminutes of each month
+			for(Text val: values){
+				line = val.toString().split(":");
+				month = Integer.parseInt(line[0]);
+				sum[month-1] += Double.parseDouble(line[1]);
+				count[month-1] ++;
+			}
+			//calculate the average delay of each month, 
+			//if there's no record in one month, set the average to NULL
+			for(int i = 0; i < 12; i++){
+				if (count[i] != 0){
+					average[i] = sum[i]/count[i];
+					resultString += ", (" + (i+1) + "," 
+									+ (int) Math.round(average[i]) + ")";
+				} else{
+					resultString += ", (" + (i+1) + ",NULL)";
+				}
+			}
+			context.write(nullkey, new Text(resultString));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
 		Configuration conf = HBaseConfiguration.create();
 		String[] otherArgs = new GenericOptionsParser(conf, args)
 				.getRemainingArgs();
 		if (otherArgs.length != 1) {
-			System.err.println("Usage: populate <in>");
+			System.err.println("Usage: compute <out>");
 			System.exit(1);
 		}
-		//create table
-		HBaseAdmin admin = new HBaseAdmin(conf);
-		HTableDescriptor htd = new HTableDescriptor(tablename);
-		HColumnDescriptor hcd = new HColumnDescriptor(delayFamily); 
-		htd.addFamily(hcd);
-		//if table exists, drop it
-		if(admin.tableExists(tablename)){
-			admin.disableTable(tablename);
-			admin.deleteTable(tablename);
-		}
-		admin.createTable(htd);
-		
-		conf.set(TableOutputFormat.OUTPUT_TABLE, tablename);
-		Job job = new Job(conf, "populate htable");
-		job.setJarByClass(HPOPULATE.class);
-		job.setMapperClass(PopulateMapper.class);
-		job.setNumReduceTasks(10); 
-		job.setReducerClass(PopulateReducer.class);
-		job.setMapOutputKeyClass(ImmutableBytesWritable.class);
-		job.setMapOutputValueClass(DoubleWritable.class);
-		job.setOutputFormatClass(TableOutputFormat.class);
+		//set the scan with startRowkey and endRowkey
+		Scan scan = new Scan(STARTROWKEY, ENDROWKEY);
+		scan.setCaching(1000);
+		scan.setCacheBlocks(false);
+		Job job = new Job(conf, "compute htable");
 
-		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		job.setJarByClass(HCOMPUTE.class);
+		TableMapReduceUtil.initTableMapperJob(tablename, scan,
+				ComputeMapper.class, Text.class, Text.class, job);
+		job.setNumReduceTasks(10);
+		job.setReducerClass(ComputeReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[0]));
 		int exitCode = job.waitForCompletion(true) ? 0 : 1;
-		admin.close();
-		return exitCode;
-	}
-
-	public static void main(String[] args) throws Exception {
-		int exitCode = run(args);
 		System.exit(exitCode);
 	}
 }
